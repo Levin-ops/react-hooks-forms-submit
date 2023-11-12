5a94ef0b35e24be90f05ee47fef595562b56c81d
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-12 14:34:29 +0300

    event listener added

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,8 +12,20 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event){
+    event.preventDefault(); // this prvents the form from makin new requests to the current page causing a refresh
+    const formData = { // putting together the current form data into an object using values stored in state.
+      firstName: firstName,
+      lastName: lastName,
+    };
+    props.sendFormDataSomewhere(formData);
+    setFirstName("");
+    setLastName("");
+  }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
