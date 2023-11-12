e75f1601e0e3ccf403b19ec1e083932b3fad154e
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-12 14:43:42 +0300

    handling submissions

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -18,18 +19,31 @@ function Form(props) {
       firstName: firstName,
       lastName: lastName,
     };
-    props.sendFormDataSomewhere(formData);
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
     setFirstName("");
     setLastName("");
   }
 
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return(
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    )
+  } )
+
 
   return (
+    <div>
     <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    <h3>Submissions</h3>
+    {listOfSubmissions}
+    </div>
   );
 }
 
